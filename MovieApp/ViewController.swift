b3e280b11cc2f2609a8e6a41e6e0f7e3fc3b0cf5
endava-bootcamp <endava-bootcamp@fives-MacBook-Pro.local>
2023-03-23 16:58:04 +0100

    first commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+import MovieAppData
+
+    class ViewController: UIViewController {
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            let details = MovieUseCase().getDetails(id: 111161)
+            print(details)
+        }
+}
+
