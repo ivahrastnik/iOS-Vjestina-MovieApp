a36cfd05a6e9ad103230228f4fbebbbd8b5a83d9
endava-bootcamp <endava-bootcamp@fives-MacBook-Pro.local>
2023-03-26 16:33:52 +0200

    First login screen done

---
+++ MovieApp/MovieDetailsViewController.swift
@@ -0,0 +1,30 @@
+import Foundation
+import UIKit
+import PureLayout
+
+class MovieDetailsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        buildViews()
+    }
+    
+    private func buildViews() {
+        createViews()
+        styleViews()
+        defineLayoutForViews()
+    }
+    
+    private func createViews(){
+        
+    }
+    
+    private func styleViews(){
+        
+    }
+    
+    private func defineLayoutForViews(){
+        
+    }
+    
+}
