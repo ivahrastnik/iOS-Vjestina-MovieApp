9451f4482c7ded6b776fcf01f05b591875039fe8
endava-bootcamp <endava-bootcamp@fives-MacBook-Pro.local>
2023-04-04 22:37:36 +0200

    commit2

--- MovieApp/MovieListViewController.swift
+++ MovieApp/MovieListViewController.swift
@@ -1,33 +1,23 @@
 import Foundation
 import UIKit
 import PureLayout
+import MovieAppData
+import Kingfisher
 
 let cellIdentifier = "cellId"
+let allMovies = MovieUseCase().allMovies
 
 class MovieListViewController: UIViewController {
     
-    
-    
     private var scrollView: UIScrollView!
     private var tableView: UITableView!
+    private var collectionView: UICollectionView!
     
     let cellIdentifier = "cellId"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.scrollDirection = .vertical
-        let collectionView = UICollectionView(
-            frame: CGRect( x: 0,
-                           y: 0,
-                           width: view.bounds.width, height: view.bounds.height),
-            collectionViewLayout: flowLayout)
-        collectionView.backgroundColor = .white
-        view.addSubview(collectionView)
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellIdentifier)
-        collectionView.dataSource = self
-        collectionView.delegate = self
+        buildViews()
     }
     
 //    override func viewDidLoad() {
@@ -47,39 +37,50 @@ class MovieListViewController: UIViewController {
 //
 //    }
     
-//    private func buildViews() {
-//        createViews()
-//        styleViews()
-//        defineLayoutForViews()
-//    }
-//
-//    private func createViews() {
-//
-////        scrollView = UIScrollView()
-////        view.addSubview(scrollView)
-////
-////        tableView = UITableView()
-////        scrollView.addSubview(tableView)
-//
-//
-//    }
+    private func buildViews() {
+        createViews()
+        styleViews()
+        defineLayoutForViews()
+    }
+
+    private func createViews() {
+
+        scrollView = UIScrollView()
+        view.addSubview(scrollView)
+        
+        i = 0
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .vertical
+        
+        collectionView = UICollectionView(
+            frame: CGRect( x: 0,
+                           y: 0,
+                           width: view.bounds.width, height: view.bounds.height),
+            collectionViewLayout: flowLayout)
+        
+        view.addSubview(collectionView)
+
+
+    }
     
-//    private func styleViews() {
-//
-//        scrollView.backgroundColor = .white
-//        tableView.backgroundColor = .systemMint
-//
-//
-//
-//
-//    }
-//
-//    private func defineLayoutForViews() {
-//
-//        scrollView.autoPinEdgesToSuperviewEdges()
-//        tableView.autoPinEdgesToSuperviewEdges()
-//
-//    }
+    private func styleViews() {
+
+        scrollView.backgroundColor = .white
+        collectionView.backgroundColor = .white
+        view.backgroundColor = .white
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellIdentifier)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+
+    private func defineLayoutForViews() {
+
+        scrollView.autoPinEdgesToSuperviewEdges()
+        collectionView.autoPinEdgesToSuperviewSafeArea()
+
+    }
     
 }
 
@@ -88,12 +89,74 @@ extension MovieListViewController: UICollectionViewDataSource {
         return 1
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return allMovies.count
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell( withReuseIdentifier: cellIdentifier,
                                                        for: indexPath)
-        cell.backgroundColor = .blue
+        cell.backgroundColor = .white
+        cell.layer.cornerRadius = 10
+        cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: 10).cgPath
+        cell.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1).cgColor
+        cell.layer.shadowRadius = 20
+        cell.layer.shadowOpacity = 1
+        cell.layer.shadowOffset = CGSize(width: 0, height: 4)
+        cell.layer.position = cell.center
+        
+        let movie = allMovies[i]
+        
+        let imgView = UIImageView()
+        imgView.kf.setImage(with: URL(string: movie.imageUrl))
+        imgView.layer.cornerRadius = 10
+        imgView.clipsToBounds = true
+        imgView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMinXMinYCorner]
+        
+        cell.addSubview(imgView)
+        imgView.autoSetDimension(.width, toSize: 97)
+        imgView.autoPinEdge(toSuperviewEdge: .top)
+        imgView.autoPinEdge(toSuperviewEdge: .bottom)
+        imgView.autoPinEdge(toSuperviewEdge: .leading)
+        
+        let nameView = UILabel()
+        let summaryView = UILabel()
+        
+//        String str = movie.name
+        
+        nameView.text = String(movie.name)
+//        summaryView.text = String(movie.summary)
+        
+        nameView.textColor = .black
+        nameView.font = UIFont(name: "ProximaNova-Bold", size: 16)
+        nameView.textAlignment = .left
+        
+        summaryView.textColor = .black
+        summaryView.font = UIFont(name: "ProximaNova-Regular", size: 14)
+        summaryView.textAlignment = .left
+        summaryView.numberOfLines = 0
+        summaryView.lineBreakMode = .byWordWrapping
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineHeightMultiple = 1.4
+        summaryView.attributedText = NSMutableAttributedString(string: movie.summary, attributes: [NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        
+        cell.addSubview(nameView)
+        cell.addSubview(summaryView)
+        
+        nameView.autoSetDimension(.height, toSize: 20)
+        nameView.autoPinEdge(toSuperviewEdge: .top, withInset: 12)
+        nameView.autoPinEdge(.leading, to: .trailing, of: imgView, withOffset: 16)
+        nameView.autoPinEdge(toSuperviewSafeArea: .trailing)
+        
+        summaryView.autoPinEdge(toSuperviewEdge: .trailing, withInset: 12)
+        summaryView.autoPinEdge(.leading, to: .trailing, of: imgView, withOffset: 16)
+        summaryView.autoPinEdge(.top, to: .bottom, of: nameView, withOffset: 8)
+        summaryView.autoPinEdge(toSuperviewEdge: .bottom, withInset: 8)
+        
+        i += 1
+        
+        if(i == allMovies.count) {
+            i = 0
+        }
+        
         return cell
     }
 }
@@ -120,7 +183,6 @@ extension MovieListViewController: UICollectionViewDelegateFlowLayout {
 //    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 //        return 100
 //    }
-//
 //    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 //        let cell: UITableViewCell = tableView.dequeueReusableCell(
 //            withIdentifier: cellIdentifier,
@@ -128,21 +190,74 @@ extension MovieListViewController: UICollectionViewDelegateFlowLayout {
 //
 //        var cellConfig: UIListContentConfiguration = cell.defaultContentConfiguration() // 5.
 //
-//        let cellView = UIView()
-//        cell.addSubview(cellView)
-//        cellView.autoSetDimension(.height, toSize: 142)
-//        cellView.autoSetDimension(.width, toSize: 358)
-//        cellView.autoPinEdge(toSuperviewEdge: .leading, withInset: 16)
-//        cellView.layer.cornerRadius = 10
-//        cell.con
+//        cell.backgroundColor = .white
+//        cell.layer.cornerRadius = 10
+//        cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: 10).cgPath
+//        cell.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1).cgColor
+//        cell.layer.shadowRadius = 20
+//        cell.layer.shadowOpacity = 1
+//        cell.layer.shadowOffset = CGSize(width: 0, height: 4)
+//        cell.layer.position = cell.center
 //
-//        cellConfig.text = "Row \(indexPath.row)"
-//        cellConfig.textProperties.color = .blue
-//        cell.contentConfiguration = cellConfig
+//        let movie = allMovies[i]
+//
+//        let imgView = UIImageView()
+//        imgView.kf.setImage(with: URL(string: movie.imageUrl))
+//        imgView.layer.cornerRadius = 10
+//        imgView.clipsToBounds = true
+//        imgView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMinXMinYCorner]
+//
+//        cell.addSubview(imgView)
+//        imgView.autoSetDimension(.width, toSize: 97)
+//        imgView.autoPinEdge(toSuperviewEdge: .top)
+//        imgView.autoPinEdge(toSuperviewEdge: .bottom)
+//        imgView.autoPinEdge(toSuperviewEdge: .leading)
+//
+//        let nameView = UILabel()
+//        let summaryView = UILabel()
+//
+//        //        String str = movie.name
+//
+//        nameView.text = String(movie.name)
+//        summaryView.text = String(movie.summary)
+//
+//        nameView.textColor = .black
+//        nameView.font = UIFont(name: "ProximaNova-Bold", size: 16)
+//        nameView.textAlignment = .left
+//
+//        summaryView.textColor = .black
+//        summaryView.font = UIFont(name: "ProximaNova-Regular", size: 14)
+//        summaryView.textAlignment = .left
+//
+//        cell.addSubview(nameView)
+//        cell.addSubview(summaryView)
+//
+//        nameView.autoSetDimension(.height, toSize: 20)
+//        nameView.autoPinEdge(toSuperviewEdge: .top, withInset: 12)
+//        nameView.autoPinEdge(.leading, to: .trailing, of: imgView, withOffset: 16)
+//        nameView.autoPinEdge(toSuperviewSafeArea: .trailing)
+//
+//        i += 1
+//
+//        if(i == allMovies.count) {
+//            i = 0
+//        }
+//
+//        //        let cellView = UIView()
+//        //        cell.addSubview(cellView)
+//        //        cellView.autoSetDimension(.height, toSize: 142)
+//        //        cellView.autoSetDimension(.width, toSize: 358)
+//        //        cellView.autoPinEdge(toSuperviewEdge: .leading, withInset: 16)
+//        //        cellView.layer.cornerRadius = 10
+//        //        cell.con
+//        //
+//        //        cellConfig.text = "Row \(indexPath.row)"
+//        //        cellConfig.textProperties.color = .blue
+//        //        cell.contentConfiguration = cellConfig
 //        return cell
 //    }
     
-//    func styleCell() {
-//
-//    }
-    
+    //    func styleCell() {
+    //
+    //    }
+//}
